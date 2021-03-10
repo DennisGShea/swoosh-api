536c2e558e3886139a922afef70dc00850ed26ed
Dennis G Shea <DennisGSheaPhD@gmail.com>
2021-03-09 21:52:28 -0500

    first api commit

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const functions = require("firebase-functions");
 const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const { getItem, postItem, patchItem, deleteItem} = require('./src/swoosh-api')
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,15 +14,9 @@ app.post('/name', postItem)
 app.delete('/name/:itemId', deleteItem)
 app.patch('/name/:itemId', patchItem)
 
-export.app = functions.https.onRequest(app)
+exports.app = functions.https.onRequest(app)
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
 
 
 
